36e500a00f47c65c0d7aacebf6379d59a70d4c5e
ruslankobrin <ruslankobrin@gmail.com>
2019-10-20 00:32:36 +0300

    Сonnected the database

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
+from config import Configuration
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+db = SQLAlchemy(app)
