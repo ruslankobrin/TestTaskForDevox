8f634bf877dfac84d7c21d378b4436c6115cd6de
ruslankobrin <ruslankobrin@gmail.com>
2019-10-20 19:33:30 +0300

    Add models "Item", "Category"

--- models.py
+++ models.py
@@ -1,11 +1,30 @@
 from app import db
 
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
     name = db.Column(db.String(50))
     age = db.Column(db.INTEGER)
     phone = db.Column(db.String(20))
 
     def __init__(self, *args, **kwargs):
-        super(Users, self).__init__(*args, **kwargs)
+        super(User, self).__init__(*args, **kwargs)
+
+
+class Category(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(50))
+    users_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        super(Category, self).__init__(*args, **kwargs)
+
+
+class Item(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(100))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        super(Item, self).__init__(*args, **kwargs)
